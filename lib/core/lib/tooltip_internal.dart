3e38623d268845ae248764865f166e2cb8f64414
xstapix <sarafannikov.85@mail.ru>
2024-02-19 11:52:40 +0300

    first commit

---
+++ lib/core/lib/tooltip_internal.dart
@@ -0,0 +1,9 @@
+library tooltip_internal;
+
+import 'dart:async';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+// ignore: depend_on_referenced_packages
+import 'core.dart';
+part 'src/tooltip/tooltip.dart';
